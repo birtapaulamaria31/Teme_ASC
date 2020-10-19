2ea67a54289f8b3788fc871b756b9f0593f628e9
birtapaulamaria31 <paulamariabirta1002@gmail.com>
2020-10-19 17:58:21 +0300

    quiz lab. 19.10

---
+++ T2_conversieBinara/T2_conversieBinara/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_conversieBinara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n = float.Parse(Console.ReadLine());
+            int a = Convert.ToInt32(Math.Floor(n));
+
+            conv(a);
+            float b = n - a;
+            if (b != 0)
+                Console.Write(".");
+            while( Math.Floor(b)!=b)
+            {
+                b = b * 2;
+                if (b >= 1f)
+                {
+                    b = b - 1f;
+                    Console.Write(1);
+                } else
+                {
+                    Console.Write(0);
+                }
+                
+
+
+            }
+
+
+            
+            Console.ReadKey();
+        }
+
+        static void conv(int n)
+        {
+            if (n != 0)
+            {
+                conv(n / 2);
+                Console.Write(n % 2);
+            }
+        }
+    }
+}
