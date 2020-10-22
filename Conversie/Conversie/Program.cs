b9fa6afc2eab1ebb5bc4254e5563d1b88cdcb1a5
birtapaulamaria31 <paulamariabirta1002@gmail.com>
2020-10-22 22:14:37 +0300

    Versiune primara, merge doar fara virgula

---
+++ Conversie/Conversie/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Mesaje introductorii
+            Console.WriteLine("Acesta este un program care transforma un numar dintr-o baza in alta.");
+            Console.WriteLine("Restrictii si precizari: bazele sunt cuprinse intre 2 si 16. Numerele sunt pozitive si pot fi cu virgula.");
+
+            /// Input
+            Console.Write("Incepe prin a introduce numarul tau: ");
+            string inputNumber = Console.ReadLine();
+
+            Console.Write("Acum baza in care este numarul tau: ");
+            int bazaInitiala = int.Parse(Console.ReadLine());
+
+            Console.Write("Si in final, baza in care vrei sa fie convertit numarul tau: ");
+            int bazaFinala = int.Parse(Console.ReadLine());
+
+            /// Impartim sirul citit in doua siruri 
+            string[] splitNumbers = inputNumber.Split('.');
+
+            /// Convertim numarul in baza 10
+            int base_10 = StringToBase10(splitNumbers[0], bazaInitiala);
+
+            /// Afisam partea intreaga a numarului final
+            Base10ToFinal(base_10, bazaFinala);
+
+            Console.ReadKey();
+        }
+
+        static int StringToBase10(string v, int baza)
+        {
+            int suma = 0;
+            int putere = 1;
+            // Parcurgem sirul invers
+            for(int i=v.Length-1; i>=0; i--)
+            {
+                // Daca este o litera
+                if((int)v[i]>=65 && (int)v[i]<=70 )
+                {
+                    suma = suma + ((int)v[i] - 55) * putere;
+                   
+                }
+                // Daca este o cifra
+                else if ((int)v[i]>=48 && (int)v[i]<=57)
+                {
+                    suma = suma + ((int)v[i]-48) * putere;
+                  
+                }
+                putere = putere * baza;
+            }
+            return suma;
+        }
+
+        static void Base10ToFinal(int n, int baza)
+        {
+            Stack<char> stiva = new Stack<char>();
+            string HEX = "0123456789ABCDEF";
+            while (n != 0)
+            {
+                int rest = n % baza;
+                stiva.Push(HEX[rest]);
+                n = n / baza;
+            }
+            while(stiva.Count()!=0)
+            {
+                Console.Write(stiva.Pop());
+            }
+        }
+
+
+    }
+}
--- T2_conversieBinara/T2_conversieBinara/Program.cs
+++ T2_conversieBinara/T2_conversieBinara/Program.cs
@@ -10,41 +10,54 @@ namespace T2_conversieBinara
     {
         static void Main(string[] args)
         {
+            int b1, b2;
+            Console.WriteLine("Introdu numarul pe care vrei sa il convertesti:");
             float n = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu baza in care se afla numarul introdus:");
+            b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu baza in care vrei sa convertesti numarul:");
+            b2 = int.Parse(Console.ReadLine());
+
             int a = Convert.ToInt32(Math.Floor(n));
 
-            conv(a);
-            float b = n - a;
-            if (b != 0)
-                Console.Write(".");
-            while( Math.Floor(b)!=b)
+            if (b1 == 10)
             {
-                b = b * 2;
-                if (b >= 1f)
+                convDin10ParteIntreaga(a, b2);
+                float b = n - a;
+                if (b != 0)
+                    Console.Write(".");
+                while (Math.Floor(b) != b)
                 {
-                    b = b - 1f;
-                    Console.Write(1);
-                } else
-                {
-                    Console.Write(0);
+                    b = b * b2;
+                    if (b >= 1f)
+                    {
+                        Console.Write(b % 10);
+                        b = b - 1f;
+
+                    }
+
+
+
                 }
-                
-
-
             }
+            
 
 
             
+
+
             Console.ReadKey();
         }
 
-        static void conv(int n)
+        static void convDin10ParteIntreaga(int n, int b2) //conversia pt partea intreaga a unui numar din baza 10
         {
             if (n != 0)
             {
-                conv(n / 2);
-                Console.Write(n % 2);
+                convDin10ParteIntreaga(n / b2, b2);
+                Console.Write(n % b2);
             }
         }
+
+        
     }
 }
